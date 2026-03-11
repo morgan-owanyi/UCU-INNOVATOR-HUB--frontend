9515b1417ff93dd7e286a3bcf69d6fc00363de9f
claudianandege <claudianandege@gmail.com>
2026-03-11 21:13:07 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+// import React from "react";
+// import Navbar from "./components/Navbar";
+
+// function App() {
+//   return (
+//     <div>
+//       <Navbar />
+
+//       <h1>Welcome to UCU Innovators Hub</h1>
+//       <p>Explore innovative student projects</p>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+
+function App() {
+
+return(
+
+<div>
+
+<Navbar/>
+
+<Login/>
+
+</div>
+
+)
+
+}
+
+export default App;
